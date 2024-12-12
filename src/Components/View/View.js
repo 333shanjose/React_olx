ba1295f53136a8d71fa848ab01cf30e4b51bc79f
shan jose <109017031+333shanjose@users.noreply.github.com>
2024-12-12 11:04:17 +0530

    Update View.js

--- src/Components/View/View.js
+++ src/Components/View/View.js
@@ -1,15 +1,18 @@
 import React, { useContext, useEffect, useState } from "react";
 import { PostContext } from "../../contextStore/PostContext";
+import { AuthContext } from "../../contextStore/AuthContext";
+
 import { Firebase } from "../../firebase/config";
 import { useHistory } from "react-router";
 import "./View.css";
 function View() {
   let { postContent } = useContext(PostContext);//from the global store PostContext we can get information about desired product post that we want to show (the user is clicked item on the card)
+     const { userId } = useContext(AuthContext);
 
   const [userDetails, setUserDetails] = useState();//we want show the details of who is posted the add and we dont know,so we want retreive user data from firebase who is posted this add
   const history = useHistory();//if user click the refresh of the page then PostContext data will be erased so it will throws an error so that time we want redirect this page to home page
   useEffect(() => {
-    let { userId } = postContent;
+    
     if (userId === undefined) {
       history.push("/");
     } else {
